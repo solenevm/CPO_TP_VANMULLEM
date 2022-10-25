005bcb580a15f41a980684f55e07c60e102c0745
Solene_Van_Mullem <solene.vml@gmail.com>
2022-10-25 11:26:34 +0200

    création attribut et méthode : initialisation nbConversions à 0 méthode conversions Celcius --> Kelvin et Kelvin --> Celcius et les autres conversions avec Fahrenheit... et nb Conversions prend bien +1 a chaque fois méthode to String + commentaires

--- TP2_convertisseurObjet_VanMullem/src/tp2_convertisseurobjet_vanmullem/Convertisseur.java
+++ TP2_convertisseurObjet_VanMullem/src/tp2_convertisseurobjet_vanmullem/Convertisseur.java
@@ -9,19 +9,48 @@ package tp2_convertisseurobjet_vanmullem;
  * @author Soso
  */
 public class Convertisseur {
+    
     int nbConversions;
-    public void Convertisseur(){
-        nbConversions = 0;
+    public void Convertisseur(){ //création constructeur
+        nbConversions = 0; //initialisation de la variable nbConversions
     }
-    public double CelciusVersKelvin(double a){
-        a=a+273;
+    public double CelciusVersKelvin(double a){ //variable double (permet d'avoir un nombre décimal)
+        a=a+273; //calcul de conversion correspondant de C° vers K
         nbConversions+=1;
-        return a;
+        return a; //retour du résultat
     }
     
+    //même principe pour chaque conversions
     public double KelvinVersCelcius(double b) {
         b=b-273;
         nbConversions+=1;
         return b;
     }
+    public double FahrenheitVersCelcius (double c) {
+        c=(c-32)/1.8;
+        nbConversions +=1;
+        return c;
+    }
+    
+    public double CelciusVersFahrenheit (double d){
+        d=(d*1.8)+32;
+        nbConversions+=1;
+        return d;
+    }
+    
+    public double FahrenheitVersKelvin (double e){
+        e=(e+459.67)*(5/9);
+        nbConversions+=1;
+        return e;       
+    }
+    
+    public double KelvinVersFahreinheit (double f) {
+        f=(f*(9/5))-459.67;
+        nbConversions+=1;
+        return f;
+    }
+    @Override
+   public String toString () {
+       return "nb de conversions" + nbConversions;
+   }
 }
