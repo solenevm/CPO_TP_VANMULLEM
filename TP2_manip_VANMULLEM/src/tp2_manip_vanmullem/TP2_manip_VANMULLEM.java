ff1659d605aef15f61013e7d38c57ab1a0479ffe
Solene_Van_Mullem <solene.vml@gmail.com>
2022-10-25 10:15:27 +0200

    création méthode tartiflette puis moussaka + attribution variable nbcalories 3 tartiflette avec calories différentes + affichages

---
+++ TP2_manip_VANMULLEM/src/tp2_manip_vanmullem/TP2_manip_VANMULLEM.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_vanmullem;
+
+/**
+ *
+ * @author Soso
+ */
+public class TP2_manip_VANMULLEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ;
+        
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+        System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories) ;
+
+    }
+    
+}
