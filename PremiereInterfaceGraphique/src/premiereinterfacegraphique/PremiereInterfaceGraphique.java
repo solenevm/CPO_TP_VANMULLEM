869c66d2b18bd828c8831ef14e2742ee1b645783
Solene_Van_Mullem <solene.vml@gmail.com>
2022-11-07 11:12:20 +0100

    Création interface choix d'achats avec trois boutons validation, une zone de texte et trois radio button (et les labels)

--- PremiereInterfaceGraphique/src/premiereinterfacegraphique/PremiereInterfaceGraphique.java
+++ PremiereInterfaceGraphique/src/premiereinterfacegraphique/PremiereInterfaceGraphique.java
@@ -13,8 +13,5 @@ public class PremiereInterfaceGraphique {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
