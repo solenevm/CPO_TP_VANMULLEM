3aae657cbd336a9b919c4cc4f959cfcd3746794d
Solene_Van_Mullem <solene.vml@gmail.com>
2022-11-08 15:07:43 +0100

    Création TP4 héritage renommé en TP3 heritage

---
+++ TP4_Heritage/src/tp4_heritage/Arme.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp4_heritage;
+
+/**
+ *
+ * @author Soso
+ */
+public class Arme {
+    String arme;
+    int NivAtt = 0;
+    
+    System.out.println("Entrer votre prénom");
+    ch = sc.nextLine();
+}
