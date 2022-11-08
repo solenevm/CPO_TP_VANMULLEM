350d41c4502ac45403e34bb3a4fa5b105eddaf8e
Solene_Van_Mullem <solene.vml@gmail.com>
2022-11-08 16:18:13 +0100

    RE CREATION fichier TP3_Heritage (suite à un bug) CREATION classe ARME, EPEE, BATON +affichage types armes dans la console

--- TP3_Heritage/src/tp3_heritage/TP3_Heritage.java
+++ TP3_Heritage/src/tp3_heritage/TP3_Heritage.java
@@ -14,6 +14,24 @@ public class TP3_Heritage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String arme;
+        int NivAtt = 0;
+        
+        Arme couteau = new Arme ("couteau", 40 );
+        System.out.println(couteau);
+        
+        Epee excalibur = new Epee ("Excalibur", 7, 5);
+        System.out.println(excalibur);
+        
+        Epee Durandal = new Epee ("Durandal", 4, 7);
+        System.out.println(Durandal);
+        
+        Baton Chene = new Baton ("Chene", 4, 5);
+        System.out.println(Chene);
+        
+        Baton Charme = new Baton ("Charme", 5, 6);
+        System.out.println(Charme);
+
         // TODO code application logic here
     }
     
